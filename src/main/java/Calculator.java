9883d42861a51622ad2ab522da8832aff1d73a7d
sumit-ingle <61662978+sumit-ingle@users.noreply.github.com>
2021-08-11 22:21:46 +0530

    Feature 1: Implement basic calculator for addition of two or less numbers

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public int add(String numbers) {
+        if (numbers.equals(""))
+            return 0;
+        if (numbers.contains(",")) {
+            String[] nums = numbers.split(",");
+            return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+        }
+        return Integer.parseInt(numbers);
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,63 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CalculatorTest {
+    @Nested
+    @DisplayName("Addition")
+    class Addition {
+        @Test
+        void returns_0_for_empty_string() {
+            Calculator calculator = new Calculator();
+            int additionResult = calculator.add("");
+            Assertions.assertEquals(0, additionResult);
+        }
+
+        @Nested
+        @DisplayName("Single Number")
+        class SingleNumber {
+            @Test
+            void returns_1_for_input_1() {
+                Calculator calculator = new Calculator();
+                int additionResult = calculator.add("1");
+                Assertions.assertEquals(1, additionResult);
+            }
+
+            @Test
+            void returns_2_for_input_2() {
+                Calculator calculator = new Calculator();
+                int additionResult = calculator.add("2");
+                Assertions.assertEquals(2, additionResult);
+            }
+
+            @ParameterizedTest
+            @ValueSource(strings = {"10", "200", "3333", "1234567890"})
+            void returns_n_for_input_n(String number) {
+                Calculator calculator = new Calculator();
+                int additionResult = calculator.add(number);
+                Assertions.assertEquals(Integer.parseInt(number), additionResult);
+            }
+        }
+
+        @Nested
+        @DisplayName("Two Numbers")
+        class TwoNumbers {
+            @Test
+            void returns_2_for_1comma1() {
+                Calculator calculator = new Calculator();
+                int additionResult = calculator.add("1,1");
+                Assertions.assertEquals(2, additionResult);
+            }
+
+            @Test
+            void returns_3_for_1comma2() {
+                Calculator calculator = new Calculator();
+                int additionResult = calculator.add("1,2");
+                Assertions.assertEquals(3, additionResult);
+            }
+        }
+    }
+}
